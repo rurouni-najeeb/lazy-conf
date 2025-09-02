6658e3bfe2f48abbebe3f02ecb2bf01ebbd5cd2d
Najeeb Khan <najeeb.khan@moia.io>
2025-09-02 16:31:34 +0200

    add key mappings for neotest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -8,12 +8,32 @@ return {
       "nvim-treesitter/nvim-treesitter",
       "nvim-neotest/neotest-python",
     },
+    keys = {
+      { "<leader>tt", function() require("neotest").run.run() end, desc = "Run nearest test" },
+      { "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run current file tests" },
+      { "<leader>td", function() require("neotest").run.run({strategy = "dap"}) end, desc = "Debug nearest test" },
+      { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle test summary" },
+      { "<leader>to", function() require("neotest").output.open({ enter = true }) end, desc = "Open test output" },
+      { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle output panel" },
+      { "<leader>tw", function() require("neotest").watch.toggle() end, desc = "Toggle watch mode" },
+    },
     config = function()
       require("neotest").setup({
         adapters = {
           require("neotest-python")({
             dap = { justMyCode = false },
+            args = {"--log-level", "DEBUG"},
+            runner = "pytest",
           }),
+        },
+        discovery = {
+          enabled = true,
+        },
+        running = {
+          concurrent = true,
+        },
+        summary = {
+          animated = true,
         },
       })
     end,
