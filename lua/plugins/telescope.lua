5f534eafd15cfab33a7c3c716ab71047c8e8ba56
Najeeb Khan <najeeb.khan@moia.io>
2025-10-09 11:33:05 +0200

    add find and live grep for hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,7 @@
 return {
   "nvim-telescope/telescope.nvim",
-  keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
+  keys = { -- add a keymap to browse plugin files
+    -- stylua: ignore
     {
       "<leader>fH",
       function()
@@ -15,6 +14,38 @@ return {
       end,
       desc = "Find Files in Home Directory",
     },
+    -- Search all files including hidden ones in current project
+    {
+      "<leader>fA",
+      function()
+        require("telescope.builtin").find_files({
+          hidden = true,
+        })
+      end,
+      desc = "Find All Files (including hidden)",
+    },
+    -- Search only Git-tracked files (including hidden)
+    {
+      "<leader>fG",
+      function()
+        require("telescope.builtin").git_files({
+          show_untracked = true,
+        })
+      end,
+      desc = "Find Git Files (including untracked)",
+    },
+    -- Live grep including hidden files
+    {
+      "<leader>sA",
+      function()
+        require("telescope.builtin").live_grep({
+          additional_args = function()
+            return { "--hidden" }
+          end,
+        })
+      end,
+      desc = "Search in All Files (including hidden)",
+    },
   },
   -- change some options
   opts = {
@@ -23,11 +54,33 @@ return {
       layout_config = { prompt_position = "top" },
       sorting_strategy = "ascending",
       winblend = 0,
-      hidden = true,
-      pickers = {
-        find_files = {
-          hidden = true,
+      mappings = {
+        i = {
+          ["<C-j>"] = "move_selection_next",
+          ["<C-k>"] = "move_selection_previous",
         },
+      },
+      file_ignore_patterns = {
+        "%.git/",
+        "node_modules/",
+        "%.DS_Store",
+        ".venv/",
+        ".mypy_cache/",
+        "__pycache__/",
+      },
+    },
+    pickers = {
+      find_files = {
+        hidden = true,
+        find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+      },
+      live_grep = {
+        additional_args = function()
+          return { "--hidden" }
+        end,
+      },
+      git_files = {
+        show_untracked = true,
       },
     },
   },
