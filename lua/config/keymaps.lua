336542122f8c52a5d1708ab986b2d7d9683a6879
Najeeb Khan <najeeb.khan@moia.io>
2025-11-10 21:05:13 +0100

    add keymap for enabling and disable copilot completions

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,18 @@
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("n", "<leader>th", ":split | terminal<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>tv", ":vsplit | terminal<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>hc", function()
+  vim.g.ai_completion_enabled = not vim.g.ai_completion_enabled
+  if vim.g.ai_completion_enabled == nil then
+    vim.g.ai_completion_enabled = false
+  end
+  if vim.g.ai_completion_enabled then
+    vim.cmd("Copilot enable")
+    vim.notify("AI completions enabled", vim.log.levels.INFO)
+  else
+    vim.cmd("Copilot disable")
+    vim.notify("AI completions disabled", vim.log.levels.INFO)
+  end
+end, { desc = "Toggle AI Completions" })
+
